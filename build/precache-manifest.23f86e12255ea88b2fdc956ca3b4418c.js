ea3b165f2666fb18596285a5997f970166c5276c
SatyamThakur30 <satyam.aiiit@gmail.com>
2021-05-18 20:44:06 +0530

    initial

---
+++ build/precache-manifest.23f86e12255ea88b2fdc956ca3b4418c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74d1991d63f5ffb9dc1eb50a5fd653a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80196a798195de928085",
+    "url": "/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "34b5a05fb9ef3d241143",
+    "url": "/static/css/main.06e3d0ab.chunk.css"
+  },
+  {
+    "revision": "80196a798195de928085",
+    "url": "/static/js/2.1a280da7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1a280da7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34b5a05fb9ef3d241143",
+    "url": "/static/js/main.d92b50d6.chunk.js"
+  },
+  {
+    "revision": "10af4fb5bc87ef5eb5a7",
+    "url": "/static/js/runtime-main.b91b79ab.js"
+  }
+]);
